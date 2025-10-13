dc85df5a483582037bf90897e4c47a9a1d5eac11
Michael McCreary <michaelmccreary95@gmail.com>
2025-10-13 18:24:47 +0100

    Project creation / basic configuration

--- app.py
+++ app.py
@@ -2,3 +2,15 @@ from models import db
 from flask import Flask , render_template, request, redirect, url_for, flash, jsonify, session
 from config import Config
 
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to CineLog</h1>'
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Create database tables
+    app.run(debug=True)
--- models.py
+++ models.py
@@ -1,3 +1,30 @@
 from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
 from werkzeug.security import generate_password_hash, check_password_hash
+db = SQLAlchemy()
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(60), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+    reviews = db.relationship('Review', backref='author', lazy=True)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    class Post(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        title = db.Column(db.String(100), nullable=False)
+        content = db.Column(db.Text, nullable=False)
+        user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+        timestamp = db.Column(db.DateTime, index=True, default=db.func.now())
+
+    class Review(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        movie_id = db.Column(db.Integer, nullable=False)
+        rating = db.Column(db.Integer, nullable=False)
+        comment = db.Column(db.Text, nullable=True)
+        user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+        timestamp = db.Column(db.DateTime, index=True, default=db.func.now())
